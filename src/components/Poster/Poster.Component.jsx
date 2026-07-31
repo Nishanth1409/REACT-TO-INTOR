a732d8bc920428ee6a4513a6e9a52900a5083c05
Nishanth1409 <nishanthkr1409@gmail.com>
2026-07-31 18:05:25 +0530

    Clamp movie poster titles so mobile layout stays clean

--- src/components/Poster/Poster.Component.jsx
+++ src/components/Poster/Poster.Component.jsx
@@ -5,13 +5,13 @@ const Poster = (props) => {
   return (
   
         <Link to={`movie/${props.id}`}>
-            <div className='flex flex-col items-start gap-2 px-1 md:px-3'>
-                <div className='h-40 md:h-80'>
-                    <img src={`https://image.tmdb.org/t/p/original${props.poster_path}` } alt="poster" 
-                    className='w-full h-full rounded-md'
+            <div className='flex flex-col items-start gap-2 px-1 md:px-3 w-full max-w-[9.5rem] md:max-w-[12.5rem]'>
+                <div className='w-full h-40 md:h-80'>
+                    <img src={`https://image.tmdb.org/t/p/original${props.poster_path}` } alt="Movie poster" 
+                    className='w-full h-full object-cover rounded-md'
                     />
                 </div>
-                <h3 className={`text-lg font-bold ${props.isDark ? 'text-white': 'text-black'}`}>{props.title}</h3>
+                <h3 className={`text-sm md:text-lg font-bold leading-snug line-clamp-2 ${props.isDark ? 'text-white': 'text-black'}`}>{props.title}</h3>
             </div>
         </Link>
   )
